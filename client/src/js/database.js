d686e0b5aa2f67199d7e6888e56d67dbac39fff8
sweinkauf <scott.weinkauf29@gmail.com>
2023-02-06 22:20:25 -0600

    fixed err

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,7 +14,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.error('putDb not implemented');
+  
   console.log('PUT to the database');
   const jateDb = await openDB ('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
@@ -28,7 +28,7 @@ export const putDb = async (content) => {
 }
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.error('getDb not implemented');
+  
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate')
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,4 +1,5 @@
 const butInstall = document.getElementById('buttonInstall');
+console.log(butInstall)
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
